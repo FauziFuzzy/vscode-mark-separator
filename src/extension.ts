fadda817078300792ff293cc32fbb5c9361d4f34
FauziFuzzy <fauzi_zulfadli@yahoo.com>
2026-05-18 16:32:08 +0800

    change trigger keyword from MARK to mm

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
-const MARK_PATTERN = /\/\/\s*MARK:\s*(.*)/;
+const MARK_PATTERN = /\/\/\s*mm:\s*(.*)/;
 
 let separatorDecorationType: vscode.TextEditorDecorationType;
 let resolvedColor: string;
